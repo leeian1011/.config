2e794c20813902bff75668b755a6dcc2ccee1f98
Lee <leeian1011@gmail.com>
2023-07-22 18:22:24 +0800

    removed harpoon and some old remaps

--- nvim/lua/nvrc/remap.lua
+++ nvim/lua/nvrc/remap.lua
@@ -1,7 +1,15 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "R", vim.cmd.redo)
-vim.keymap.set('n', '<leader>pv', ':NvimTreeToggle<CR>', { noremap = true, silent = true, nowait = true })
--- Remap to shift nvim current working director
-vim.keymap.set("n", "<leader>c", ":cd %:h<CR>")
+-- BUFFER NAVIGATION
+-- NEXT BUFFER
+vim.keymap.set('n', "<C-n>", ':bn<CR>')
+-- PREVIOUS BUFFE
+vim.keymap.set('n', "<C-p>", ':bp<CR>')
+-- CLOSE BUFFER
+vim.keymap.set('n', "<C-x>", ':bd<CR>')
 
+-- REDO
+vim.keymap.set("n", "R", vim.cmd.redo)
+
+-- NVIM TREE OPEN
+vim.keymap.set('n', '<leader>pv', ':NvimTreeToggle<CR>', { noremap = true, silent = true, nowait = true })
